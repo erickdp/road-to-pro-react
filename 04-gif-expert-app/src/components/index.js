fc98049d3848b82e3de210e8b9e3eb94bc18659e
Erick Díaz <erickdp@hotmail.com>
2023-02-21 21:34:04 -0500

    clean files

--- 04-gif-expert-app/src/components/index.js
+++
@@ -1,4 +0,0 @@
-// tambien denominado como archivo barril, permite agrupar las exportaciones y sea más ordenado la importación
-export * from './AddCategory';
-export * from './GifGrid';
-export * from './GifItem';
