d7f7a093734d44f9b0ab87985aa75fdbacd63dde
Erick Díaz <erickdp@hotmail.com>
2023-02-21 21:05:04 -0500

    update http api

--- 04-gif-expert-app/src/helpers/getGifs.js
+++ 04-gif-expert-app/src/helpers/getGifs.js
@@ -1,7 +1,7 @@
 export const getGifs = async (category) => {
   const apiKey = "Q1qErt6x2OX6YgYiRwElBFmOIaxrRzz7";
 
-  const url = `http://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${category}&limit=20`;
+  const url = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${category}&limit=20`;
 
   const resp = await fetch(url);
 
